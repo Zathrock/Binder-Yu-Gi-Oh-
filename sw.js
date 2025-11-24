c28e97fb72cc86e735be580aa5db2fadd10a0ce8
Zathrock <cryptex666.vdf@gmail.com>
2025-11-25 00:26:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "binder-shell-v14";
+const CACHE_NAME = "binder-shell-v15";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -43,6 +43,7 @@ self.addEventListener("fetch", (e) => {
   );
 
 });
+
 
 
 
